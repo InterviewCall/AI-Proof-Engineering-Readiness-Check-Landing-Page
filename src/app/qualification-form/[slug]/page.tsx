a933a44c9766ae91452c4c5b3ba448faacd95812
suvajit04 <suvajit.sahoo.22@aot.edu.in>
2026-09-14 14:01:24 +0530

    r

--- src/app/qualification-form/[slug]/page.tsx
+++ src/app/qualification-form/[slug]/page.tsx
@@ -1,17 +1,32 @@
+import { notFound } from 'next/navigation';
 import { FC } from 'react';
 
 import CandidateQualificationForm from '@/components/qualification-form/CandidateQualificationForm';
+import { landingPages } from '@/constants/landingPage';
 
 type QualificationFormSlugPageProps = {
     params: Promise<{
         slug: string
+    }>,
+    searchParams: Promise<{
+        'candidate-id'?: string
     }>
 };
 
-const QualificationSlugPage: FC<QualificationFormSlugPageProps> = async ({ params }) => {
+const QualificationSlugPage: FC<QualificationFormSlugPageProps> = async ({ params, searchParams }) => {
     const { slug } = await params;
+    const { 'candidate-id': candidateId } = await searchParams;
+    const landingPage = landingPages[slug as keyof typeof landingPages];
+
+    if (!landingPage) {
+        notFound();
+    }
+
     return (
-        <CandidateQualificationForm slug={slug} />
+        <CandidateQualificationForm
+            slug={landingPage.formSlug}
+            candidateId={candidateId}
+        />
     );
 };
 
