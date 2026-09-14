39f5b15939eed5440356f9a9e91020751b2048a9
levi8102 <iamgourabdg2002@gmail.com>
2026-09-14 15:13:58 +0530

    fixed book-strategy-call

---
+++ src/app/[slug]/book-strategy-call/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from 'next/navigation';
+
+import BookingPage from '@/components/booking-page/BookingPage';
+import { bookingPageContentBySlug } from '@/constants/bookingPageContent';
+import { landingPages, LandingPageSlug } from '@/constants/landingPage';
+
+type BookStrategyCallPageProps = {
+    params: Promise<{
+        slug: string
+    }>
+};
+
+const BookStrategyCallPage = async ({ params }: BookStrategyCallPageProps) => {
+    const { slug } = await params;
+
+    if(!(slug in landingPages)) {
+        notFound();
+    }
+
+    const landingPageData = landingPages[slug as LandingPageSlug];
+    const content = bookingPageContentBySlug[landingPageData.formSlug];
+
+    if(!content) {
+        notFound();
+    }
+
+    return (
+        <BookingPage
+            content={content}
+            slug={landingPageData.formSlug}
+        />
+    );
+};
+
+export default BookStrategyCallPage;
